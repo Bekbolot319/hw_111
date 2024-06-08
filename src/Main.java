86ccec01744d00740c491482eec9267b4a99e013
Bekbolot319 <beksattarob42@gmail.com>
2024-06-08 20:15:03 +0600

    jh

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String carsNumber;
+        final int NUM = 34;
+        String word = "двигатель";
+                carsNumber = NUM + word;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println(NUM);
+        System.out.println(carsNumber);
+        System.out.println(word);
+
+        if (NUM < 0) {
+            System.out.println("Вы сохранили отрицвтельное число");
+        } else if (NUM > 0) {
+            System.out.println("Вы сохранили положительное число");
+        } else {
+            System.out.println("Вы сохранили ноль");
         }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите ваше имя");
+
+        String name = scanner.nextLine();
+        System.out.println("привет " + name + "!");
+
     }
 }
